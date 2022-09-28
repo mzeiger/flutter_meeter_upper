61a3c25e9b19d219ffb234841995579a9ddaf29a
Mark Zeiger <mark.zeiger@gmail.com>
2022-09-27 19:48:45 -0600

    Start of tutorial

--- lib/src/screens/counter_home_screen.dart
+++ lib/src/screens/counter_home_screen.dart
@@ -5,10 +5,8 @@ class CounterHomeScreen extends StatefulWidget {
 
   const CounterHomeScreen({super.key, required String title}) : _title = title;
 
-  @override
-  CounterHomeScreenState createState() {
-    return CounterHomeScreenState();
-  }
+  @override // arrow function always returns
+  CounterHomeScreenState createState() => CounterHomeScreenState();
 }
 
 class CounterHomeScreenState extends State<CounterHomeScreen> {
@@ -23,21 +21,6 @@ class CounterHomeScreenState extends State<CounterHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _increment();
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      appBar: AppBar(
-        title: Text(widget._title),
-        centerTitle: true,
-        elevation: 10,
-        backgroundColor: Colors.green,
-        titleTextStyle: const TextStyle(
-            color: Colors.red, fontSize: 30, fontWeight: FontWeight.bold),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -50,6 +33,57 @@ class CounterHomeScreenState extends State<CounterHomeScreen> {
           ],
         ),
       ),
+      appBar: AppBar(
+        title: Text(widget._title),
+        centerTitle: true,
+        elevation: 10,
+        backgroundColor: Colors.green,
+        titleTextStyle: const TextStyle(
+            color: Colors.red, fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+      bottomNavigationBar: _BottomNavigation(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _increment();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _BottomNavigation extends StatefulWidget {
+  @override
+  _BottomNavigationState createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<_BottomNavigation> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      onTap: (int index) {
+        setState(() => _currentIndex = index);
+      },
+      items: const [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.white),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+            backgroundColor: Colors.white),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.white),
+      ],
+      backgroundColor: Colors.grey,
+      showSelectedLabels: true,
     );
   }
 }
