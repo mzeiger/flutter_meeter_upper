72e6072474ec77881ed907c2b827b8ac441988e2
Mark Zeiger <mark.zeiger@gmail.com>
2022-10-02 20:09:01 -0600

    Broker cardTemplate function into it's own file. Changed background color of cards

--- lib/src/screens/kiwanis_screen.dart
+++ lib/src/screens/kiwanis_screen.dart
@@ -3,6 +3,8 @@ import '../widgets/bottom_navigation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import '../widgets/kiwanis_card_widget.dart';
+
 class KiwanisScreen extends StatefulWidget {
   const KiwanisScreen({super.key});
 
@@ -27,35 +29,6 @@ class KiwanisScreenState extends State<KiwanisScreen> {
       final posts = json.decode(res.body);
       setState(() => _posts = posts);
     });
-  }
-
-  Widget cardTemplate(post) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-      child: Container(
-        decoration: const BoxDecoration(color: Color(0xFF444488),),
-        padding: const EdgeInsets.only(bottom: 10, left: 5, right: 5),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text("${post['lastname']}, ${post['firstname']}",
-            style: const TextStyle(color: Colors.blue, fontSize: 30),),
-            Text("Spouse: ${post['cb_spousename']}",
-              style: const TextStyle(color: Colors.green, fontSize: 20),),
-            Text("${post['email']}",
-              style: const TextStyle(color: Colors.cyan, fontSize: 20),),
-            Text("${post['cb_address']}",
-              style: const TextStyle(color: Colors.blue, fontSize: 20),),
-            Text("${post['cb_city']}, ${post['cb_state']} ${post['cb_zipcode']}",
-              style: const TextStyle(color: Colors.blue, fontSize: 20),),
-            Text("Home Phone:  ${post['cb_homephone']}",
-              style: const TextStyle(color: Colors.blue, fontSize: 20),),
-            Text("Cell Phone: ${post['cb_mobilephone']}",
-              style: const TextStyle(color: Colors.blue, fontSize: 20),),
-          ],
-        ),
-      ),
-    );
   }
 
   @override
---
+++ lib/src/widgets/kiwanis_card_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+Widget cardTemplate(post) {
+  return Card(
+    margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+    child: Container(
+      decoration: const BoxDecoration(color: Color(0xFFd1d1d1),),
+      padding: const EdgeInsets.only(bottom: 10, left: 5, right: 5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text("${post['lastname']}, ${post['firstname']}",
+            style: const TextStyle(color: Colors.blue, fontSize: 30),),
+          Text("Spouse: ${post['cb_spousename']}",
+            style: const TextStyle(color: Colors.green, fontSize: 20),),
+          Text("${post['email']}",
+            style: const TextStyle(color: Colors.cyan, fontSize: 20),),
+          Text("${post['cb_address']}",
+            style: const TextStyle(color: Colors.blue, fontSize: 20),),
+          Text("${post['cb_city']}, ${post['cb_state']} ${post['cb_zipcode']}",
+            style: const TextStyle(color: Colors.blue, fontSize: 20),),
+          Text("Home Phone:  ${post['cb_homephone']}",
+            style: const TextStyle(color: Colors.blue, fontSize: 20),),
+          Text("Cell Phone: ${post['cb_mobilephone']}",
+            style: const TextStyle(color: Colors.blue, fontSize: 20),),
+        ],
+      ),
+    ),
+  );
+}
